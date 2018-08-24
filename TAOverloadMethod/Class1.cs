65e2c2adbe54f22a3a995752a56c0a2f679904c1
Keenan Carroll <keenan.carroll.va@gmail.com>
2018-08-24 14:35:55 -0400

    Ref and Out test

---
+++ TAOverloadMethod/Class1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TAOverloadMethod
+{
+    class Number
+    {
+        public void NumOne(int num1)
+        {
+            int result = num1 / 2;
+            Console.WriteLine("Your number divided by 2 is : {0}", result);
+        }
+        public void valueOut(out int x)
+        {
+            int temp = 5;
+            x = temp;
+        }
+        public void NumOne(int num1, int num2)
+        {
+            int result = num1 + num2;
+        }
+    }
+    public static class Math
+    {
+
+    }
+}
--- TAOverloadMethod/Program.cs
+++ TAOverloadMethod/Program.cs
@@ -10,6 +10,20 @@ namespace TAOverloadMethod
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Feed me a number!");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+
+            Number div = new Number();
+            div.NumOne(num1);
+
+            int x = 100;
+
+            Number valueOut = new Number();
+            valueOut.valueOut(out x);
+
+            Console.WriteLine("Out is : {0}", x);
+
+            Console.ReadLine();
         }
     }
 }
